1dfa9ef4c9ccc301d33e5d135070b437360a5935
miguel_pereira <miguelglp90@gmail.com>
2021-02-07 15:56:06 +0000

    exercicio 1 aula 4

--- src/moduloLaboratorio/aula4/exercicio1/AMinhaTarefa.java
+++ src/moduloLaboratorio/aula4/exercicio1/AMinhaTarefa.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AMinhaTarefa implements Runnable{
+    private List<String> listaPalavras;
+
+    public AMinhaTarefa(List<String> p) {
+        this.listaPalavras = p;
+    }
 
     @Override
     public void run() {
@@ -15,22 +20,14 @@ public class AMinhaTarefa implements Runnable{
 
     public void readMy() {
 
-        List<String> listaPalavras = new ArrayList<>();
+        for (int i = 0; i < listaPalavras.size(); i++) {
+            String word = listaPalavras.get(i);
+            String cap = word.substring(0, 1).toUpperCase() + word.substring(1);
+            listaPalavras.set(i, cap);
+        }
 
-        try {
-            File myObj = new File("C:\\Users\\Utilizador\\IdeaProjects\\Novo1\\src\\moduloLaboratorio\\aula4" +
-                    "\\exercicio1\\fiche.txt");
-            Scanner myReader = new Scanner(myObj);
-            myReader.useDelimiter(" ");
-            while (myReader.hasNext()) {
-                String word = myReader.next();
-                   String cap = word.substring(0, 1).toUpperCase() + word.substring(1);
-                   listaPalavras.add(cap);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        for (String l : listaPalavras) {
+            //System.out.println(l);
         }
     }
 }
--- src/moduloLaboratorio/aula4/exercicio1/Main.java
+++ src/moduloLaboratorio/aula4/exercicio1/Main.java
@@ -1,26 +1,52 @@
 package moduloLaboratorio.aula4.exercicio1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void main(String[] args) {
-        AMinhaTarefa t1 = new AMinhaTarefa();
-        AMinhaTarefa t2 = new AMinhaTarefa();
-        AMinhaTarefa t3 = new AMinhaTarefa();
-        AMinhaTarefa t4 = new AMinhaTarefa();
-        AMinhaTarefa t5 = new AMinhaTarefa();
-        AMinhaTarefa t6 = new AMinhaTarefa();
+        List<String> listaCompleta = new ArrayList<>();
+        int count = 0;
 
+
+        try {
+            File myObj = new File("C:\\Users\\Utilizador\\IdeaProjects\\Novo1\\src\\moduloLaboratorio\\aula4" +
+                    "\\exercicio1\\fiche.txt");
+            Scanner myReader = new Scanner(myObj);
+            myReader.useDelimiter(" ");
+            while (myReader.hasNext()) {
+                listaCompleta.add(myReader.next());
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
         ExecutorService pool = Executors.newFixedThreadPool(6);
 
-        pool.submit(t1);
-        pool.submit(t2);
-        pool.submit(t3);
-        pool.submit(t4);
-        pool.submit(t5);
-        pool.submit(t6);
+        for (int i = 0; i < listaCompleta.size(); i = i + 200) { //a lista tem 783 palavras, mas ainda faz as quatro revoluções antes de parar
+            List<String> subLista;
+
+            if (i + 200 < listaCompleta.size()) {
+                subLista = listaCompleta.subList(i, i + 200);
+            } else {
+                subLista = listaCompleta.subList(i, listaCompleta.size());
+            }
+
+            AMinhaTarefa t1 = new AMinhaTarefa(subLista);
+            pool.submit(t1);
+
+            count++;
+        }
+
+        System.out.println(count);
     }
 }
